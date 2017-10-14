f349f6c6ea631be1d410c6d75a5f9d6db3b171f3
cwz <517364893@qq.com>
2017-10-14 10:51:51 +0800

    Initial commit

---
+++ src/main/java/j/HbaseConf.java
@@ -0,0 +1,26 @@
+package j;
+
+import org.apache.hadoop.conf.Configuration;
+
+/**
+ * Author: cwz
+ * Time: 2017/9/20
+ * Description: 设置hbase连接配置
+ */
+public class HbaseConf {
+    // 配置hbase的主节点ip
+    private final static String master = "192.168.0.41";
+    // 配置hbase的zookeeper节点ip
+    private final static String zookeeper = "192.168.0.41,192.168.0.42,192.168.0.43";
+    // 配置hbase的zookeeper连接端口，默认值为2181
+    private final static String port = "2181";
+
+    public static Configuration getConf() {
+        Configuration hbaseConf = new Configuration();
+        hbaseConf.set("hbase.master", master);
+        hbaseConf.set("hbase.zookeeper.quorum", zookeeper);
+        hbaseConf.set("hbase.zookeeper.property.clientPort", port);
+        return hbaseConf;
+    }
+
+}
